e4f1843d5e40408183dc96bd5288614b78bd5725
Ana Delia Torres <34944025+anapumilio@users.noreply.github.com>
2023-04-04 14:33:34 +0200

    Update streamlit_app.py
    add new options to the menu and a new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
 streamlit.header('Breakfast Menu')
-streamlit.text('Kale & Oatmeal')
+
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streanlit.text('🥑🍞 Avocado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
